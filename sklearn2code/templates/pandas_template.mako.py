946c3a2bb2c80d91e5dd34524ef8649aa60c11ae
Matt Lewis <mattrlewis2@gmail.com>
2018-03-27 17:45:19 -0700

    adding weighted mode to pandas template

--- sklearn2code/templates/pandas_template.mako.py
+++ sklearn2code/templates/pandas_template.mako.py
@@ -1,8 +1,17 @@
-from numpy import vectorize, array, bincount, argmax, apply_along_axis, equal, where, isnan, maximum, minimum, exp, logical_not, logical_and, logical_or, select, less_equal, greater_equal, less, greater, nan, inf, log, asarray
+from numpy import arange, newaxis, cumsum, vectorize, array, bincount, argmax, apply_along_axis, asarray, array, argmax, argsort, transpose, bincount, equal, where, isnan, maximum, minimum, exp, logical_not, logical_and, logical_or, select, less_equal, greater_equal, less, greater, nan, inf, log
 from scipy.special import expit
 from pandas import DataFrame
 from toolz import compose
 from functools import partial
+
+def weighted_median(data, weights):
+    data = data.T
+    sorted_idx = argsort(data, axis=1)
+    weight_cdf = cumsum(weights[sorted_idx], axis=1)
+    median_or_above = weight_cdf >= 0.5 * weight_cdf[:, -1][:, newaxis]
+    median_idx = median_or_above.argmax(axis=1)
+    medians = sorted_idx[arange(data.shape[0]), median_idx]
+    return data[arange(data.shape[0]), medians]
 
 <%!
 from toolz import flip
